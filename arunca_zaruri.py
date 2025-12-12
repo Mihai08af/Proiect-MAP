9642f2514a5f206e25a899121ce75c7a0f28e52b
Costea Mihai <mihai-daniel.costea@student.upt.ro>
2025-12-12 16:29:36 +0200

    Update arunca_zaruri.py

--- arunca_zaruri.py
+++ arunca_zaruri.py
@@ -1,16 +1,16 @@
-"Setez seed-ul Random number generator"
+"""Setez seed-ul Random number generator"""
 def set_seed(seed: int | None):
     if seed is not None:
         random.seed(seed)
 
-"Sa arunce un zar"
+"""Sa arunce un zar"""
 def roll_dice(faces: int) -> int:
     return random.randint(1, faces)
 
-"Sa arunce zaruri"
+"""Sa arunce zaruri"""
 def roll_dices(dice: int, faces: int) -> List[int]:
     return [roll_dice(faces) for _ in range(dice)]
 
-"Sa arunce zaruri de 'rolls' ori"
+"""Sa arunce zaruri de 'rolls' ori"""
 def roll_many(rolls: int, faces:int) -> List[int]:
     return [roll_dice(faces) for _ in range(rolls)]
