f18f4c1f72669012f516243e95082cf5e6ac820a
Costea Mihai <mihai-daniel.costea@student.upt.ro>
2025-12-12 16:29:39 +0200

    Update statistici.py

--- statistici.py
+++ statistici.py
@@ -1,4 +1,4 @@
-"media, mediana si cat de imprastiate/apropaite sunt rezultatele"
+"""media, mediana si cat de imprastiate/apropaite sunt rezultatele"""
 def basic_stats(samples: List[int]) -> Tuple[float, float, float]:
     if not samples:
         return (float("nan"), float("nan"), float("nan"))
