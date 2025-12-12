bacb7e3d96babaf0a7443a21864011b7614b7b58
Costea Mihai <mihai-daniel.costea@student.upt.ro>
2025-12-12 15:59:39 +0200

    Create statistici.py
    Cotele practic (:

---
+++ statistici.py
@@ -0,0 +1,8 @@
+"media, mediana si cat de imprastiate/apropaite sunt rezultatele"
+def basic_stats(samples: List[int]) -> Tuple[float, float, float]:
+    if not samples:
+        return (float("nan"), float("nan"), float("nan"))
+    mean = statistics.fmean(samples)
+    median = statistics.median(samples)
+    stdev = statistics.pstdev(samples)
+    return (mean, median, stdev)
